74cfc65952c67c45aa6389f21f5f79c3f7ddd7e4
KavyaRamasamy16 <136969293+KavyaRamasamy16@users.noreply.github.com>
2024-07-08 13:00:36 +0530

    Add files via upload

---
+++ timeconversion.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = "10";
+
+//b = Number(b);    //function to convert string to number    or use the below method parse
+
+b =parseInt(b);
+
+console.log(typeof a);
+
+console.log(a==b);
+console.log(a===b);
